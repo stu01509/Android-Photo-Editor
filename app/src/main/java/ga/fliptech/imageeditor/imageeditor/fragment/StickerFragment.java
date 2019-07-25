0e29065a66bc64565577b48b8ea4a1442e305d36
Cliff Su <stu01509@yahoo.com.tw>
2019-07-25 23:34:49 +0800

    feat: remove no neccessary code.

--- app/src/main/java/ga/fliptech/imageeditor/imageeditor/fragment/StickerFragment.java
+++ app/src/main/java/ga/fliptech/imageeditor/imageeditor/fragment/StickerFragment.java
@@ -16,62 +16,20 @@ import android.widget.Toast;
 import ga.fliptech.imageeditor.MainActivity;
 import ga.fliptech.imageeditor.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link StickerFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link StickerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class StickerFragment extends Fragment {
 
-    private static final String TAG = "Fragment1";
-
-    private Button btnNavFrag1;
-    private Button btnNavFrag2;
-    private Button btnNavFrag3;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-//    private OnFragmentInteractionListener mListener;
-
     public StickerFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StickerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StickerFragment newInstance(String param1, String param2) {
+    public static StickerFragment newInstance() {
         StickerFragment fragment = new StickerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -80,75 +38,13 @@ public class StickerFragment extends Fragment {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_sticker, container, false);
         View view  = inflater.inflate(R.layout.fragment_sticker, container, false);
-        btnNavFrag1 = (Button) view.findViewById(R.id.btnNavFrag1);
-        btnNavFrag2 = (Button) view.findViewById(R.id.btnNavFrag2);
-        btnNavFrag3 = (Button) view.findViewById(R.id.btnNavFrag3);
-        Log.d(TAG, "onCreateView: started.");
-
-        btnNavFrag1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Going to Fragment 1", Toast.LENGTH_SHORT).show();
-
-                ((MainActivity)getActivity()).setMode(0);
-            }
-        });
-
-        btnNavFrag2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Going to Fragment 2", Toast.LENGTH_SHORT).show();
-                ((MainActivity)getActivity()).setMode(1);
-            }
-        });
-
-        btnNavFrag3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Going to Fragment 3", Toast.LENGTH_SHORT).show();
-                ((MainActivity)getActivity()).setMode(2);
-            }
-        });
-
         return view;
     }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
+
+//    public void finishEdit() {
+//        MainActivity.isEdit = false;
+//        MainActivity.rvModeList.setVisibility(View.INVISIBLE);
+//        MainActivity.modeViewPager.setVisibility(View.VISIBLE);
 //    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+
 }
