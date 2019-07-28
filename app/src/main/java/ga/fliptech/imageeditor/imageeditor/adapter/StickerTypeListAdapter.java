a4cb6c3ab84776a34e4d7b4400687f2f1cd015f0
Cliff Su <stu01509@yahoo.com.tw>
2019-07-28 19:49:18 +0800

    style: remove newline.

--- app/src/main/java/ga/fliptech/imageeditor/imageeditor/adapter/StickerTypeListAdapter.java
+++ app/src/main/java/ga/fliptech/imageeditor/imageeditor/adapter/StickerTypeListAdapter.java
@@ -1,17 +1,13 @@
 package ga.fliptech.imageeditor.imageeditor.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import ga.fliptech.imageeditor.MainActivity;
 import ga.fliptech.imageeditor.R;
 import ga.fliptech.imageeditor.imageeditor.fragment.StickerFragment;
 
