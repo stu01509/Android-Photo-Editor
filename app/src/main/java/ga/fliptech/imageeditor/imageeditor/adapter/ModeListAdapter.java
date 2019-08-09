a491479f500eb574d02dbbc5b93edb77f443fbac
Cliff Su <stu01509@yahoo.com.tw>
2019-08-09 23:24:39 +0800

    feat: change mode list icon.

--- app/src/main/java/ga/fliptech/imageeditor/imageeditor/adapter/ModeListAdapter.java
+++ app/src/main/java/ga/fliptech/imageeditor/imageeditor/adapter/ModeListAdapter.java
@@ -1,29 +1,24 @@
 package ga.fliptech.imageeditor.imageeditor.adapter;
 
 import android.content.Context;
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
-
-import ga.fliptech.imageeditor.MainActivity;
+import ga.fliptech.imageeditor.EditActivity;
 import ga.fliptech.imageeditor.R;
 
 public class ModeListAdapter extends RecyclerView.Adapter {
-    private static final String TAG = "ModeListAdapter";
     private Context context;
     private final String[] modeName = {"貼圖模式", "繪圖模式", "濾鏡模式", "文字模式", "美膚模式", "旋轉模式"};
     private final int[] modeIcon = {
-            R.drawable.baseline_insert_photo_black_48, R.drawable.baseline_insert_photo_black_48,
-            R.drawable.baseline_insert_photo_black_48, R.drawable.baseline_insert_photo_black_48,
-            R.drawable.baseline_insert_photo_black_48, R.drawable.baseline_insert_photo_black_48};
+            R.drawable.ic_insert_emoticon_black_24dp, R.drawable.ic_create_black_24dp,
+            R.drawable.ic_filter_black_24dp, R.drawable.ic_format_color_text_black_24dp,
+            R.drawable.ic_face_black_24dp, R.drawable.ic_crop_rotate_black_24dp};
 
     public ModeListAdapter(Context mContext) {
         this.context = mContext;
@@ -62,7 +57,7 @@ public class ModeListAdapter extends RecyclerView.Adapter {
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    MainActivity.setMode(getAdapterPosition());
+                    ((EditActivity)context).setMode(getAdapterPosition());
                 }
             });
         }
